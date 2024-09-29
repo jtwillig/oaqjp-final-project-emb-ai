e066304c348c15fc007e2bbd92731bee762427e4
Jereme Willig <jereme@jeremewillig.com>
2024-09-29 14:19:00 -0400

    added formatting and dominant_motion

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,11 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     req_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = req_json, headers = headers)
-    return response.text
+    resp_json = json.loads(response.text)
+    resp_body = resp_json['emotionPredictions'][0]['emotion']
+    most_dominant = 0
+    for emotion in list(resp_body.keys()):
+        if resp_body[emotion] > most_dominant:
+            most_dominant = resp_body[emotion]
+            resp_body['dominant_emotion'] = emotion
+    return resp_body
