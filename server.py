5c4fed1fccf2f1a9ae63534df1555291736d1e90
Jereme Willig <jereme@jeremewillig.com>
2024-09-29 16:13:44 -0400

    addressed pylint issues on server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +9,21 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotiondetector():
+    ''' This function calculates emotion scores for text '''
     response = emotion_detector(request.args.get('textToAnalyze'))
     dominant_emotion = response['dominant_emotion']
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is <b>{dominant_emotion}</b>"
+
+    return f"""For the given statement, the system response is
+    'anger': {response['anger']}, 'disgust': {response['disgust']}, 
+    'fear': {response['fear']}, 'joy': {response['joy']} 
+    and 'sadness': {response['sadness']}. 
+    The dominant emotion is <b>{dominant_emotion}</b>"""
 
 @app.route("/")
 def render_index_page():
+    ''' This function render index.html '''
     return render_template('index.html')
 
 
